f8180adef4724ae10e7cf7f3b221bac02ba05fe3
Frankz <franciscojavier.jerez@gmail.com>
2019-09-17 02:26:20 -0300

    add WORKDIR directory

--- bpkg-template.sh
+++ bpkg-template.sh
@@ -12,13 +12,16 @@ function input () {
 function bpkg-template () {
   # pedimos el nombre de la app
   # input ${input_var_name} ${message prompt}
+  WORKDIR=$(pwd)
   input APP_NAME "Insert name for your App: "
+  echo "Create app in $WORKDIR/bpkg-$APP_NAME"
   input LICENSE "Insert kind of License: "
   
   # eso sirve para crear un directorio en el lugar donde estamos, con el nombre "bpkg-$NOMBRE_DE_LA_APP"
   # dentro creamos los archivos que corresponden como base
-  mkdir "bpkg-$APP_NAME" 
-  cd "bpkg-$APP_NAME" 
+  cd $WORKDIR
+  mkdir $WORKDIR"/bpkg-$APP_NAME" 
+  cd $WORKDIR"/bpkg-$APP_NAME" 
   touch example.sh && touch LICENSE && touch Makefile && touch package.json && touch $APP_NAME.sh
   curl -sS -Lo- https://opensource.org/licenses/MIT | grep -A 10  "Begin license text" | grep  -v "Begin license text." | grep -v LicenseText | sed 's/<[^>]*>//g' | sed "s|&lt;YEAR&gt;|$(date +\"%Y\")|g" | sed "s|&lt;COPYRIGHT HOLDER&gt;|\"$(whoami)\"|g" > LICENSE
   
